a32c9ef60c1505a40f687ebcea96cd57f17e8657
Denis Voropaev <d.n.voropaev@gmail.com>
2022-08-31 00:03:45 +0300

    added blink animation on click

--- src/App/Context.tsx
+++ src/App/Context.tsx
@@ -18,7 +18,7 @@ export const EyeArrayContext = createContext<EyeArrayProps>({
     height: 44,
     eyesInRow: 1,
     eyesInCol: 1,
-    setScale: () => undefined
+    setScale: () => undefined,
 });
 
 export const useEyeArrayContext = () => useContext(EyeArrayContext);
@@ -34,7 +34,7 @@ export const ProvideEyeArrayContext= ({children}: {children: ReactNode}) => {
         2 * window.innerHeight / (scale * height) + 1
     ), [height]);
 
-    const [scale, setScale] = useState<number>(2);
+    const [scale, setScale] = useState<number>(8);
     const [eyesInRow, setEyesInRow] = useState<number>(getEyesInRow());
     const [eyesInCol, setEyesInCol] = useState<number>(getEyesInCol());
 
@@ -49,7 +49,7 @@ export const ProvideEyeArrayContext= ({children}: {children: ReactNode}) => {
         width,
         height,
         eyesInRow,
-        eyesInCol
+        eyesInCol,
     };
 
     return <EyeArrayContext.Provider value={context}>{children}</EyeArrayContext.Provider>;
--- src/Eye/Context.tsx
+++ src/Eye/Context.tsx
@@ -1,14 +1,40 @@
 'use strict';
 
 import * as React from 'react';
-import {createContext, useContext, ReactNode} from 'react';
+import {
+    createContext,
+    useContext,
+    useEffect,
+    RefObject,
+    useRef,
+    HTMLAttributes,
+    FunctionComponent,
+} from 'react';
 
-export const EyeContext = createContext({
+export type EyeProps = HTMLAttributes<SVGElement> & {
+    prefix?: string;
+};
+
+export type EyeContext = {
+    irisColor: string,
+    skinColor: string,
+    pupilScale: number,
+    corneaCenterX: number,
+    corneaCenterY: number,
+    upperEyelidBlink: RefObject<SVGAnimateElement>;
+    lowerEyelidBlink: RefObject<SVGAnimateElement>;
+    idPrefix: string;
+};
+
+export const EyeContext = createContext<EyeContext>({
     irisColor: '',
     skinColor: '',
     pupilScale: 1,
     corneaCenterX: 0,
     corneaCenterY: 0,
+    upperEyelidBlink: {current: null},
+    lowerEyelidBlink: {current: null},
+    idPrefix: ''
 });
 
 export const useEyeContext = () => useContext(EyeContext);
@@ -48,13 +74,32 @@ const skinColors = [
     '#593123',
 ];
 
-export const ProvideEyeContext = ({children}: {children: ReactNode}) => {
+export const rescale = (x: number, xMin: number, xMax: number, yMin: number, yMax: number) => (
+    ((x - xMin) * (yMax - yMin) / (xMax - xMin)) + yMin
+);
+
+export const ProvideEyeContext: FunctionComponent<EyeProps> = ({children, prefix}) => {
+    const idPrefix = `svg-eye-${prefix}`;
     const irisColor = irisColors[Math.floor(Math.random() * irisColors.length)];
     const skinColor = skinColors[Math.floor(Math.random() * skinColors.length)];
 
-    const pupilScale = Math.min(1, Math.max(0.25, Number(Math.random().toFixed(2))));
+    const pupilScale = Number(rescale(Math.random(), 0, 1, 0.25, 1).toFixed(2));
     const corneaCenterX = 0;
     const corneaCenterY = 0;
+
+    useEffect(() => {
+        addEventListener('click', (event: MouseEvent) => {
+            if (!(event.target as HTMLElement)?.closest(`#${idPrefix}-eye-group`)) {
+                return;
+            }
+
+            upperEyelidBlink.current?.beginElement();
+            lowerEyelidBlink.current?.beginElement();
+        });
+    }, []);
+
+    const upperEyelidBlink = useRef<SVGAnimateElement>(null);
+    const lowerEyelidBlink = useRef<SVGAnimateElement>(null);
 
     const context = {
         irisColor,
@@ -62,6 +107,9 @@ export const ProvideEyeContext = ({children}: {children: ReactNode}) => {
         pupilScale,
         corneaCenterX,
         corneaCenterY,
+        upperEyelidBlink,
+        lowerEyelidBlink,
+        idPrefix,
     };
 
     return <EyeContext.Provider value={context}>{children}</EyeContext.Provider>;
